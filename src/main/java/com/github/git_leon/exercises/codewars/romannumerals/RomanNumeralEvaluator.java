b28905e66d16939b099f63b0fa94846f06a91672
git-leon <xleonhunter@gmail.com>
2021-05-15 08:43:00 -0400

    created skeleton for roman numerals challenge

--- src/main/java/com/github/git_leon/exercises/codewars/romannumerals/RomanNumeralEvaluator.java
+++ src/main/java/com/github/git_leon/exercises/codewars/romannumerals/RomanNumeralEvaluator.java
@@ -1,11 +1,11 @@
 package com.github.git_leon.exercises.codewars.romannumerals;
 
-public class RomanNumerals {
+public class RomanNumeralEvaluator {
     public static String toRoman(int n) {
         return null;
     }
 
-    public static Integer fromRoman(String romanNumeral) {
+    public static Integer fromRoman(String romanNumeralExpression) {
         return null;
     }
 }
