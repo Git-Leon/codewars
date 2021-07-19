4b7c1b918e6316faa16e321742ba21bd448e0546
git-leon <xleonhunter@gmail.com>
2021-07-19 16:56:09 -0400

    added hackerrank diagnol delta q

---
+++ src/test/java/com/github/git_leon/exercisetests/DiagonalDeltaTest.java
@@ -0,0 +1,64 @@
+package com.github.git_leon.exercisetests;
+
+import com.github.git_leon.diagnoldelta.DiagonalDelta;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DiagonalDeltaTest {
+    private void getLeftDiagonalSumTest(List<List<Integer>> list, int expected) {
+        //given
+        DiagonalDelta delta = new DiagonalDelta();
+
+        //when
+        int actual = delta.getLeftDiagonalSum(list);
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getLeftDiagonalSumTest1() {
+        getLeftDiagonalSumTest(Arrays.asList(
+                Arrays.asList(11, 2, 4),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(10, 8, -12)), 4);
+    }
+
+    @Test
+    public void getLeftDiagonalSumTest2() {
+        getLeftDiagonalSumTest(Arrays.asList(
+                Arrays.asList(10, 2, 4),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(10, 8, -10)), 5);
+    }
+
+    private void getRightDiagonalSumTest(List<List<Integer>> list, int expected) {
+        //given
+        DiagonalDelta delta = new DiagonalDelta();
+
+        //when
+        int actual = delta.getRightDiagonalSum(list);
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getRightDiagonalSumTest1() {
+        getRightDiagonalSumTest(Arrays.asList(
+                Arrays.asList(11, 2, 4),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(10, 8, -12)), 19);
+    }
+
+    @Test
+    public void getRightDiagonalSumTest2() {
+        getRightDiagonalSumTest(Arrays.asList(
+                Arrays.asList(10, 2, 4),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(-10, 8, -10)), -1);
+    }
+}
